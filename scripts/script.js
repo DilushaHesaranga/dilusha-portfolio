d68fc308825f67770f43119a687149ff32eb9c19
dilushahesaranga <dilushahesaranga@gmail.com>
2026-04-21 18:22:45 +0530

    designed and developed index.html  halfway

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+let titles = [,];
+let hero_title = document.getElementById("hero-title");
+
+function display_title_01(){
+    if(hero_title.textContent=="Web Developer"){
+    hero_title.textContent="UI/UX designer";}
+    else {
+        hero_title.textContent="Web Developer";
+    }
+}
+display_title_01();
+setInterval(display_title_01,4000);
